a22d207435e750998699311999591d04dff4908d
TangYaNan <18839673302@163.com>
2018-09-18 20:51:58 +0800

    api-update

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+/*
+包含多个接口请求函数的模块 函数返回值还是promise
+ */
+
+import ajax from './ajax'
+
+const configBase = '';
+
+//请求注册接口
+export const reqRegister = ({username,password,type} )=> ajax(configBase +'/register',{username,password,type},'POST');
+
+
+//请求登录接口
+export const reqLogin = ({username,password} )=> ajax(configBase + '/login',{username,password},'POST');
